25e020218d68237a098ce6da098e37f98e8b5fba
strAngel <buynovskiy.va@yandex.ru>
2025-03-31 20:07:46 +0300

    v2

--- gen_coeffs.c
+++ gen_coeffs.c
@@ -11,17 +11,16 @@ int main() {
     printf("Введите количество наборов коэффициентов: ");
     scanf("%d", &n);
 
-    // Открываем файл для записи
     FILE *file = fopen("coefficients.txt", "w");
     if (file == NULL) {
         printf("Ошибка открытия файла!\n");
         return 1;
     }
 
-    srand(time(NULL)); // Инициализация генератора случайных чисел
+    srand(time(NULL));
 
     for (int i = 0; i < n; i++) {
-        double a = random_double(1.0, 10.0); // 'a' не должно быть 0
+        double a = random_double(1.0, 10.0);
         double b = random_double(-10.0, 10.0);
         double c = random_double(-10.0, 10.0);
         fprintf(file, "%.2lf %.2lf %.2lf\n", a, b, c);
